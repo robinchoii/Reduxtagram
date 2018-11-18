e8018f780a1c9ac947d35f160b5a56d4665b2d5f
Robin Choi <robinchoii@gmail.com>
2018-11-17 21:38:07 -0800

    initialize project

---
+++ learn-redux/client/reduxstagram.js
@@ -0,0 +1,7 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import css from './styles/style.styl';
+
+render(<p>Hello World!</p>, document.getElementById('root'));
+
