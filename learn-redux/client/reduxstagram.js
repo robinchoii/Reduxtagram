184912e002d202ceeeea40c35b6633c6233d9457
Robin Choi <robinchoii@gmail.com>
2018-11-27 17:07:32 -0800

    combine reducers

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,4 +1,3 @@
-// let's go!
 import React from 'react';
 import { render } from 'react-dom';
 import css from './styles/style.styl';
@@ -6,15 +5,20 @@ import Main from './components/Main';
 import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
 
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history }  from './store';
+
 
 const router = (
-    <Router history={browserHistory} >
-        <Route path='/' component={Main}>
-            <IndexRoute component={PhotoGrid}></IndexRoute>
-            <Route path="/view/:postId" component={Single}></Route>
-        </Route>
-    </Router>
+    <Provider store={store}>
+        <Router history={history} >
+            <Route path='/' component={Main}>
+                <IndexRoute component={PhotoGrid}></IndexRoute>
+                <Route path="/view/:postId" component={Single}></Route>
+            </Route>
+        </Router>
+    </Provider>
 )
 render(router, document.getElementById('root'));
 
