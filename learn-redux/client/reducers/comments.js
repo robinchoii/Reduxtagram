366d58f50551686268bb9e4d28cb86597fe5331f
robinchoii <robinchoii@gmail.com>
2018-11-29 22:48:55 -0800

    add comment, remove comment. updating comment state in store

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -2,9 +2,32 @@
 // 1. the action ( info about what happened)
 // 2. copy of current state
 
+function postComments(state=[], action) {
+    switch(action.type) {
+        case "ADD_COMMENT":
+            return [...state, {
+                user: action.author,
+                text: action.comment
+            }]
+        case "REMOVE_COMMENT":
+            return [
+                ...state.slice(0,action.i),
+                ...state.slice(action.i+1)
+            ]
+        default:
+            return state;
+    }
+    return state;
+}
 
 function comments(state=[], action) {
-    return state;
+    if(typeof action.postId !== 'undefined') {
+        return {
+            ...state,
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
+   return state;
 }
 
 export default comments;
